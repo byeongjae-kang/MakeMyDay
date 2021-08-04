72c8c716184b78b7731cd9eada8edebece472875
Byeongjae Kang <bart.kang.0719@gmail.com>
2021-08-03 21:15:38 -0700

    add schema for projects and user_projects

--- back-end/db/schema/projects.sql
+++ back-end/db/schema/projects.sql
@@ -1,5 +1,19 @@
-DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS projects CASCADE;
+
 CREATE TABLE projects (
-id SERIAL PRIMARY KEY,
-title VARCHAR(255) NOT NULL,
-)
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  description TEXT,
+  status VARCHAR(255) DEFAULT 'In Progress',
+  start_date TIMESTAMP DEFAULT now(),
+  due_date TIMESTAMP NOT NULL,
+  modified_date TIMESTAMP,
+  end_date TIMESTAMP
+);
+
+DROP TABLE IF EXISTS user_projects CASCADE;
+
+CREATE TABLE user_projects (
+  project_id INTEGER NOT NULL REFERENCES projects(id) ON DELETE CASCADE,
+  user_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE
+);

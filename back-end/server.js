67e350d05ff2b66fcb18db27bd85c2ce96c7e807
Byeongjae Kang <bart.kang.0719@gmail.com>
2021-08-03 21:18:00 -0700

    implement dotenv

--- back-end/server.js
+++ back-end/server.js
@@ -1,6 +1,7 @@
+require("dotenv").config();
 const express = require("express");
-const cors = require("cors");
-const pool = require("./db/db")
+// const cors = require("cors");
+const pool = require("./db/db");
 
 //App Config
 const app = express();
@@ -8,47 +9,65 @@ const port = process.env.PORT || 8080;
 
 //Middleware
 app.use(express.json());
-app.use(cors());
+// app.use(cors());
 
 //DB Config
 
-
 //API Endpoints
-app.get('/', (req, res) => {
-  res.status(200).send("connection is made")
+app.get("/", (req, res) => {
+  res.status(200).send("connection is made");
+});
+
+app.get("/users", (req, res) => {
+  pool.query("SELECT * FROM users")
+    .then((result) => {
+      res.json(result);
+    })
+    .catch((err) => {
+      console.log("helloalsdkjf;asdlkfja;sdlfkja;sdlfjka;fj", err);
+      res.status(500).send(err.message);
+      pool.end();
+    });
 });
 
 app.post("/api/users", async (req, res) => {
   try {
     const { first_name, last_name, email, password } = req.body;
-    const newUser = await pool.query("INSERT INTO users (first_name, last_name, email, password) VALUES($1, $2, $3, $4)", [first_name, last_name, email, password]);
+    const newUser = await pool.query(
+      "INSERT INTO users (first_name, last_name, email, password) VALUES($1, $2, $3, $4)",
+      [first_name, last_name, email, password]
+    );
     res.json(newUser);
   } catch (err) {
-    console.error(err.message)
+    console.error(err.message);
   }
-})
+});
 
 app.post("/api/tasks", async (req, res) => {
   try {
-    const { name, description, priority, status, start_date, due_date } = req.body;
-    const newUser = await pool.query("INSERT INTO tasks (name, description, priority, status, start_date, due_date) VALUES($1, $2, $3, $4, $5, $6)", [name, description, priority, "In Progress", start_date, due_date]);
-    
+    const { name, description, priority, status, start_date, due_date } =
+      req.body;
+    const newUser = await pool.query(
+      "INSERT INTO tasks (name, description, priority, status, start_date, due_date) VALUES($1, $2, $3, $4, $5, $6)",
+      [name, description, priority, "In Progress", start_date, due_date]
+    );
+
     res.json(newUser);
   } catch (err) {
-    console.error(err.message)
+    console.error(err.message);
   }
-})
+});
 
 app.get("/api/tasks", async (req, res) => {
   try {
     const tasks = await pool.query("SELECT * FROM tasks;");
-    res.json(tasks.rows)
+    res.json(tasks.rows);
   } catch (err) {
-    ``
-    console.error(err.message)
-    res.status(500).send(err)
+    ``;
+    console.error(err.message);
+    res.status(500).send(err);
   }
-})
+});
 
 //Listener
 app.listen(port, () => console.log(`listening on localhost:${port}`));

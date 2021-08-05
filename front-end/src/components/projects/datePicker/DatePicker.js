fa44bc7ac8f661c02d9acc8db5433e93c1dfed82
Byeongjae Kang <bart.kang.0719@gmail.com>
2021-08-05 11:05:09 -0700

    add project create feature

---
+++ front-end/src/components/projects/datePicker/DatePicker.js
@@ -0,0 +1,23 @@
+import React from "react";
+import TextField from "@material-ui/core/TextField";
+import { useStyles } from "./DatePickerStyle";
+
+export default function DatePickers({ selectedDate, handleDateChange }) {
+  const classes = useStyles();
+
+  return (
+    <form className={classes.container} noValidate>
+      <TextField
+        id="date"
+        label="Due Date"
+        type="date"
+        defaultValue={selectedDate}
+        onChange={(e) => handleDateChange(e.target.value)}
+        className={classes.textField}
+        InputLabelProps={{
+          shrink: true
+        }}
+      />
+    </form>
+  );
+}
---
+++ front-end/src/components/projects/userSelector/UserSelectorStyle.js
@@ -0,0 +1,22 @@
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: 500,
+    color: "secondary",
+    "& > * + *": {
+      marginTop: theme.spacing(3)
+    }
+  },
+  members: {
+    display: "flex",
+    alignItems: "center"
+  },
+  avatar: {
+    width: 30,
+    height: 30,
+    marginRight: 20
+  }
+}));
+
+export { useStyles }

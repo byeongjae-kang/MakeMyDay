fbb509e95a6bb9b876a13981ced00e8770eddca0
Byeongjae Kang <bart.kang.0719@gmail.com>
2021-08-09 02:18:10 -0700

    implement chat features

--- front-end/src/components/projects/chat/Chat.js
+++ front-end/src/components/projects/chat/Chat.js
@@ -1,22 +1,17 @@
-import React from 'react';
-import Button from '@material-ui/core/Button';
-import Dialog from '@material-ui/core/Dialog';
-import DialogActions from '@material-ui/core/DialogActions';
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
-import DialogTitle from '@material-ui/core/DialogTitle';
-import Slide from '@material-ui/core/Slide';
-import QuestionAnswerIcon from '@material-ui/icons/QuestionAnswer';
-
-
+import React from "react";
+import Dialog from "@material-ui/core/Dialog";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import Slide from "@material-ui/core/Slide";
+import QuestionAnswerIcon from "@material-ui/icons/QuestionAnswer";
+import Message from "./message/Message";
+import Members from "./members/Members";
+import "./Chat.css";
 const Transition = React.forwardRef(function Transition(props, ref) {
   return <Slide direction="up" ref={ref} {...props} />;
 });
 
-export default function Chat({project}) {
+export default function Chat({ project }) {
   const [open, setOpen] = React.useState(false);
-
-  console.log(project)
 
   const handleClickOpen = () => {
     setOpen(true);
@@ -29,29 +24,22 @@ export default function Chat({project}) {
   return (
     <div>
       <QuestionAnswerIcon color="secondary" onClick={handleClickOpen} />
-      
       <Dialog
         open={open}
         TransitionComponent={Transition}
         keepMounted
         onClose={handleClose}
-        aria-labelledby="alert-dialog-slide-title"
-        aria-describedby="alert-dialog-slide-description"
       >
-        <DialogTitle id="alert-dialog-slide-title">chatroom</DialogTitle>
-        <DialogContent>
-          <DialogContentText id="alert-dialog-slide-description">
-            hello
-          </DialogContentText>
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleClose} color="primary">
-            send
-          </Button>
-          <Button onClick={handleClose} color="primary">
-            Agree
-          </Button>
-        </DialogActions>
+        <DialogTitle id="alert-dialog-slide-title">
+          Chat Room: {project.name}
+        </DialogTitle>
+
+        <div className="divider">
+          <div className="project_chat_content">
+            <Members usersInProject={project.users} />
+            <Message project={project}/>
+          </div>
+        </div>
       </Dialog>
     </div>
   );
---
+++ front-end/src/components/projects/chat/message/Message.js
@@ -0,0 +1,97 @@
+import axios from "axios";
+import { AuthContext } from "context/AuthContext";
+import { useContext, useEffect, useRef, useState } from "react";
+import { format } from "timeago.js";
+import "./message.css";
+
+const getMessagesWithUsers = (messages, usersInProject) => {
+  return messages.map((message) => {
+    message["user"] = usersInProject.find(
+      (user) => user.id === message.user_id
+    );
+    delete message.user_id;
+    return message;
+  });
+};
+
+export default function Message({ project }) {
+  const [inputMessage, setInputMessage] = useState("");
+  const [allMessages, setAllMessages] = useState([]);
+  const scrollRef = useRef();
+  const { user } = useContext(AuthContext);
+
+  useEffect(() => {
+    axios
+      .get(`/api/projects/${project.id}/messages`)
+      .then((result) => {
+        const messages = getMessagesWithUsers(result.data, project.users);
+        setAllMessages([...messages]);
+      })
+      .catch((err) => console.log("could not get messages", err.message));
+  }, [user.id, project.id, project.users]);
+
+  useEffect(() => {
+    scrollRef.current?.scrollIntoView({ behavior: "smooth" });
+  }, [allMessages]);
+
+  const submitHandler = (e, user, project, message) => {
+    e.preventDefault();
+    setInputMessage("");
+
+    const newMessage = {
+      userId: user.id,
+      projectId: project.id,
+      message: message
+    };
+
+    axios
+      .post(`/api/projects/${project.id}/messages`, newMessage)
+      .then((result) => {
+        const receivedMessage = result.data[0];
+        receivedMessage["user"] = user;
+        delete receivedMessage.user_id;
+        setAllMessages([...allMessages, receivedMessage]);
+      })
+      .catch((err) =>
+        console.log("could not add message to database", err.message)
+      );
+  };
+
+  return (
+    <div className="project_messages">
+      <div className="all_project_messages">
+        {allMessages.map((message, index) => (
+          <div key={index} ref={scrollRef}>
+            <div className="each_project_message">
+              <div>
+                <img src={message.user?.avatar} alt={message.user?.user_name} />
+              </div>
+              <div className="project_message_user">
+                <p>{message.user?.user_name}</p>
+                <p>{message.message}</p>
+              </div>
+            </div>
+            <p className="project_message_time">
+              Sent: {format(message.sent_time)}
+            </p>
+          </div>
+        ))}
+      </div>
+
+      <div>
+        <form onSubmit={(e) => submitHandler(e, user, project, inputMessage)}>
+          <textarea
+            className="chat_message_input"
+            type="text"
+            placeholder="write something..."
+            value={inputMessage}
+            onChange={(e) => {
+              setInputMessage(e.target.value);
+            }}
+          />
+          <button type="submit">Send</button>
+        </form>
+      </div>
+    </div>
+  );
+}

87f6210a197237c086dc8a921187d8829643cd57
Byeongjae Kang <bart.kang.0719@gmail.com>
2021-08-10 23:18:07 -0700

    implement filtering feature

--- front-end/src/components/projects/projectList/ProjectList.js
+++ front-end/src/components/projects/projectList/ProjectList.js
@@ -14,8 +14,12 @@ import {
   FormControlLabel,
   FormLabel,
   Radio,
-  RadioGroup
+  RadioGroup,
+  TextField
 } from "@material-ui/core";
+import UserSelector from "../userSelector/UserSelector";
+import Autocomplete from "@material-ui/lab/Autocomplete";
+import { wrap } from "lodash";
 
 export default function ProjectList() {
   const {
@@ -43,29 +47,43 @@ export default function ProjectList() {
 
   const [filteredProjects, setFilteredProjects] = useState(null);
   const [filteredProcess, setFilteredProcess] = useState(null);
+  const [filteredUser, setFilteredUser] = useState(null);
 
-  const filterByUser = (e) => {
+  const resetAllFilter = () => {
+    setFilteredProjects(null)
+    setFilteredProcess(null)
+    setFilteredUser(null)
+  }
+
+  const filterByUser = (value) => {
     setFilteredProjects(null);
-    const userId = e.target.getAttribute("alt");
-    const newProjects = [...state.projects].filter((project) => {
+    setFilteredProcess(null)
+    setFilteredUser(value)
+    
+    const userId = value?.id
+    const newProjects = [...state.projects].reverse().filter((project) => {
       const foundId = project.users.find((id) => id === parseInt(userId));
       return foundId === parseInt(userId);
     });
     setFilteredProjects(newProjects);
   };
 
-  const filterByStatus = (e) => {
-    const status = e.target.value;
+  const filterByStatus = (status) => {
+    setFilteredProjects(null);
+    setFilteredUser(null)
+    setFilteredProcess(status)
     console.log(status);
-    const newProjects = [...state.projects].filter((project) => {
+    const newProjects = [...state.projects].reverse().filter((project) => {
       return status && project.status.toLowerCase() === status.toLowerCase();
     });
     setFilteredProjects(newProjects);
   };
 
-  const projects = filteredProjects ? filteredProjects : [...state.projects];
+  const projects = filteredProjects
+    ? filteredProjects
+    : [...state.projects].reverse();
 
-  const sortedProjects = projects.reverse().map((project, index) => (
+  const sortedProjects = projects.map((project, index) => (
     <div key={index}>
       <ProjectListItems
         project={HaveProjectWithUsers(project, state.users)}
@@ -79,6 +97,7 @@ export default function ProjectList() {
     <div>
       <div className="project_top_section">
         <Button
+          style={{minWidth: "220px"}}
           variant="contained"
           value={"Create"}
           color="primary"
@@ -105,58 +124,70 @@ export default function ProjectList() {
           getUserIds={getUserIds}
           userId={userId}
         />
+        <RadioGroup
+          className="filter_by_status_radio_row"
+          style={{flexDirection: 'row', flexWrap: 'none' }}
+          value={filteredProcess}
+          onChange={(e) => filterByStatus(e.target.value)}
+        >
+          <FormControlLabel
+            value="In progress"
+            control={<Radio />}
+            label="In progress"
+          />
+          <FormControlLabel
+            value="Completed"
+            control={<Radio />}
+            label="Completed"
+          />
+          <FormControlLabel
+            value="On hold"
+            control={<Radio />}
+            label="On hold"
+          />
+          <FormControlLabel
+            value="Cancelled"
+            control={<Radio />}
+            label="Cancelled"
+          />
+        </RadioGroup>
         <div className="filter_by_users">
           <label>
-            <h4>Filter By Users</h4>
+            <h4>Filter By User</h4>
           </label>
           <div className="filter_by_users_Info">
-            {state.users.map((user, index) => {
-              return (
-                <img
-                  key={index}
-                  className="filter_by_users_avatar avatar"
-                  src={user.avatar}
-                  alt={user.id}
-                  onClick={filterByUser}
+            <Autocomplete
+              onChange={(event, value) => filterByUser(value)}
+              onClick={filterByUser}
+              id="multiple-limit-tags"
+              options={state.users}
+              value={filteredUser}
+              getOptionLabel={(user) => user.user_name}
+              renderOption={(user) => (
+                <div className='filter_by_users_options'>
+                  <img className="avatar" alt={user.name} src={user.avatar} />
+                  <p>
+                    {user.user_name}
+                    <br />
+                    {user.email}
+                  </p>
+                </div>
+              )}
+              renderInput={(params) => (
+                <TextField
+                  {...params}
+                  variant="outlined"
+                  label="Search members"
+                  placeholder="Search members"
+                  color="secondary"
                 />
-              );
-            })}
+              )}
+            />
           </div>
         </div>
-        <div>
-          <FormControl className='filter_by_status'>
-            <h4>Filter by Status</h4>
-            <RadioGroup
-              className="filter_by_status_radio_row"
-              style={{flexDirection: "row" }}
-              value={filteredProcess}
-              onChange={(e) => setFilteredProcess(e.target.value)}
-              onClick={filterByStatus}
-            >
-              <FormControlLabel
-                value="In progress"
-                control={<Radio />}
-                label="In progress"
-              />
-              <FormControlLabel
-                value="Completed"
-                control={<Radio />}
-                label="Completed"
-              />
-              <FormControlLabel
-                value="On hold"
-                control={<Radio />}
-                label="On hold"
-              />
-              <FormControlLabel
-                value="Cancelled"
-                control={<Radio />}
-                label="Cancelled"
-              />
-            </RadioGroup>
-          </FormControl>
-        </div>
+        <Button onClick={resetAllFilter} variant='outlined' color='primary' style={{marginLeft: '5px'}}>RESET</Button>
       </div>
+
       <br />
       <MessageContextProvider>
         <Masonry
